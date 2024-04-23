c62e18915c47d566a22d511bf2b22b56c36665f1
Matheus Reis <theus.h.ass.reis@gmail.com>
2024-04-23 19:39:51 -0300

    fix: create absolute path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { Input } from '@/components/Input';
-
 
 export default function App() {
   return (
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,4 +3,4 @@ import styled from 'styled-components';
 export const Input = styled.input`
   border: 2px solid black;
   border-radius: 6px;
-`
+`;
