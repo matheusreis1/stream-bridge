d76e26b4a766ec465e483d38bd4308a1143195ea
Matheus Reis <theus.h.ass.reis@gmail.com>
2024-05-08 21:08:21 -0300

    feat: include images to login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Pressable, Image } from 'react-native';
 import { Input } from '@/components/Input';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Input />
+
+      <View style={{ flexDirection: 'row' }}>
+        <Pressable style={[styles.button, { marginRight: 10 }]}>
+          <Image source={{ uri: 'https://store-images.s-microsoft.com/image/apps.62962.14205055896346606.c235e3d6-fbce-45bb-9051-4be6c2ecba8f.28d7c3cb-0c64-40dc-9f24-53326f80a6dd?h=464' }} style={styles.image} />
+        </Pressable>
+
+        <Pressable style={[styles.button, { marginRight: 10 }]}>
+          <Image source={{ uri: 'https://store-images.s-microsoft.com/image/apps.62962.14205055896346606.c235e3d6-fbce-45bb-9051-4be6c2ecba8f.28d7c3cb-0c64-40dc-9f24-53326f80a6dd?h=464' }} style={styles.image} />
+        </Pressable>
+
+        <Pressable style={styles.button}>
+          <Image source={{ uri: 'https://store-images.s-microsoft.com/image/apps.62962.14205055896346606.c235e3d6-fbce-45bb-9051-4be6c2ecba8f.28d7c3cb-0c64-40dc-9f24-53326f80a6dd?h=464' }} style={styles.image} />
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -16,5 +30,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    marginVertical: 10,
+  },
+  image: {
+    width: 28,
+    height: 28,
   },
 });
